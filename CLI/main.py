14a9564aa8bb35e2ce85e69269cdbdac587193c6
AmiraliTanabian <79582085+AmiraliTanabian@users.noreply.github.com>
2025-01-15 16:25:56 +0330

    Update main.py

--- CLI/main.py
+++ CLI/main.py
@@ -1,1 +1,32 @@
 
+def Calculator():
+    num1 = float(input('Enter your Number 1 :'))
+    print('''This Calcultor can Calc 4 main operator 
+    for Sum Enter +
+    For Division Enter / 
+    For Submission Enter -
+    For Submission Enter *
+    please Enter in down line:''')
+    Operator = input('Enter your math opertor :')
+    num2 = float(input('Enter your Number 2 :'))
+    if Operator == '+':
+        print('Result :', num1 + num2)
+    if Operator == '-':
+        print( 'Result :', num1 - num2)
+    if Operator == '/':
+        print('Result :', num1 / num2)
+    if Operator == '*':
+        print('Result : ','''
+        ************''',num1 * num2)
+    Again()        
+def Again() :
+    Num_again = input('''Do you want to run the program again? 
+    Y for Yes
+    N for No | :''')
+    if Num_again.upper() == 'Y' : 
+        Calculator()
+    elif Num_again.upper() == 'N' :
+        print('Bye Bye')
+    else : 
+        Calculator()
+Calculator()
